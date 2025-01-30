9d6b0636a91163e11ab8b84bc767efe0242ba88f
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2025-01-30 16:59:11 -0400

    added 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Module: 0-add
+
+This module contains a simple function to
+perform addition between two floating-point numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Parameters:
+    a (float): The first number to add.
+    b (float): The second number to add.
+
+    Returns:
+    float: The sum of the two numbers.
+
+    Example Usage:
+    >>> result = add(5.5, 4.5)
+    >>> print(result)
+    10.0
+    """
+    return a + b
