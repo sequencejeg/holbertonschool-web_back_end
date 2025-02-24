ac9c8e15efac251635a55f29bd90be81271a1b23
sequencejeg <jeremy.etiennegonzalez@gmail.com>
2025-02-23 20:22:15 -0400

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
-""" Log stats """
+"""Log stats"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def helper(a: dict) -> int:
+    """return log"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    logs = client.logs.nginx
+    return logs.count_documents(a)
 
-    count_logs = db_nginx.count_documents({})
-    print(f'{count_logs} logs')
 
-    print('Methods:')
-    for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
+def main():
+    """ provides some stats about Nginx logs stored in MongoDB """
+    print(f"{helper({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {helper({'method': 'GET'})}")
+    print(f"\tmethod POST: {helper({'method': 'POST'})}")
+    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
+    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
 
-    check = db_nginx.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
 
-    print(f'{check} status check')
+if __name__ == "__main__":
+    main()
+    
